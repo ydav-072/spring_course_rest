d524d4a240bb648d357437e55d12677a5d671a7d
ydav <ydav-07@mail.ru>
2021-09-08 15:53:18 +0300

    delete, update Employee

--- src/main/java/com/springApp/springMVC/REST/controller/MyRESTController.java
+++ src/main/java/com/springApp/springMVC/REST/controller/MyRESTController.java
@@ -34,10 +34,26 @@ public class MyRESTController {
         return employee;
     }
 
-    @PostMapping("/employees/add")
+    @PostMapping("/employees")
     public Employee addNewEmployee(@RequestBody Employee employee){
         employeeService.saveEmployee(employee);
         return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null){
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in Database");
+        }
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted";
     }
 
 //    @ExceptionHandler // EmployeeIncorrectData added in body response
