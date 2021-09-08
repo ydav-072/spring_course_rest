b98f040fa200cd20075a3e2e757186fe3a327479
ydav <ydav-07@mail.ru>
2021-09-08 10:01:00 +0300

    show 1 employee, annotation @PathVariable

--- src/main/java/com/springApp/springMVC/REST/controller/MyRESTController.java
+++ src/main/java/com/springApp/springMVC/REST/controller/MyRESTController.java
@@ -4,10 +4,10 @@ import com.springApp.springMVC.REST.entity.Employee;
 import com.springApp.springMVC.REST.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,6 +21,12 @@ public class MyRESTController {
     public List<Employee> showAllEmployees(){
         List<Employee> employeeList = employeeService.getAllEmployees();
         return employeeList;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+        return employee;
     }
 
 }
